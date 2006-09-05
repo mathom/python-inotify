2556af653866266dceca4df819716df50f56357a
Bryan O'Sullivan <bos@pathscale.com>
2006-09-04 22:09:57 -0700

    Add add_watch function.

--- inotify/_inotify.c
+++ inotify/_inotify.c
@@ -6,14 +6,12 @@ static PyObject *pyinotify_init(PyObject *self, PyObject *args)
     PyObject *ret = NULL;
     int fd = -1;
     
-    if (!PyArg_ParseTuple(args, ":init"))
+     if (!PyArg_ParseTuple(args, ":init"))
 	goto bail;
     
     fd = inotify_init();
-
     if (fd == -1) {
-	printf("bail\n");
-	ret = PyErr_SetFromErrno(PyExc_OSError);
+	PyErr_SetFromErrno(PyExc_OSError);
 	goto bail;
     }
 	
@@ -33,12 +31,61 @@ done:
     return ret;
 }
 
-static char init_doc[] =
+PyDoc_STRVAR(
+    init_doc,
+    "init() -> fd\n"
+    "\n"
     "Initialise an inotify instance.\n"
-    "Returns a file descriptor associated with a new inotify event queue.";
+    "Return a file descriptor associated with a new inotify event queue.");
+
+static PyObject *pyinotify_add_watch(PyObject *self, PyObject *args)
+{
+    PyObject *ret = NULL;
+    uint32_t mask;
+    int wd = -1;
+    char *path;
+    int fd;
+
+    if (!PyArg_ParseTuple(args, "isI:add_watch", &fd, &path, &mask))
+	goto bail;
+
+    wd = inotify_add_watch(fd, path, mask);
+    if (wd == -1) {
+	PyErr_SetFromErrnoWithFilename(PyExc_OSError, path);
+	goto bail;
+    }
+    
+    ret = PyInt_FromLong(wd);
+    if (ret == NULL)
+	goto bail;
+    
+    goto done;
+    
+bail:
+    if (wd != -1)
+	inotify_rm_watch(fd, wd);
+    
+    Py_XDECREF(ret);
+
+done:
+    return ret;
+}
+
+PyDoc_STRVAR(
+    add_watch_doc,
+    "add_watch(fd, path, mask) -> wd\n"
+    "\n"
+    "Add a watch to an initialised inotify instance.\n"
+    "\n"
+    "        fd: file descriptor returned by init()\n"
+    "        path: path to watch\n"
+    "        mask: mask of events to watch for\n"
+    "\n"
+    "Return a numeric watch descriptor.");
 
 static PyMethodDef methods[] = {
     {"init", pyinotify_init, METH_VARARGS, init_doc},
+    {"add_watch", pyinotify_add_watch, METH_VARARGS, add_watch_doc},
     {NULL},
 };
     
