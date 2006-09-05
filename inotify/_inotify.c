7f2be85d57cc47cd7f2a2137e611633afe6915b9
Bryan O'Sullivan <bos@pathscale.com>
2006-09-04 22:23:26 -0700

    Add remove_watch.

--- inotify/_inotify.c
+++ inotify/_inotify.c
@@ -1,7 +1,7 @@
 #include <Python.h>
 #include <sys/inotify.h>
 
-static PyObject *pyinotify_init(PyObject *self, PyObject *args)
+static PyObject *init(PyObject *self, PyObject *args)
 {
     PyObject *ret = NULL;
     int fd = -1;
@@ -38,7 +38,7 @@ PyDoc_STRVAR(
     "Initialise an inotify instance.\n"
     "Return a file descriptor associated with a new inotify event queue.");
 
-static PyObject *pyinotify_add_watch(PyObject *self, PyObject *args)
+static PyObject *add_watch(PyObject *self, PyObject *args)
 {
     PyObject *ret = NULL;
     uint32_t mask;
@@ -75,17 +75,59 @@ PyDoc_STRVAR(
     add_watch_doc,
     "add_watch(fd, path, mask) -> wd\n"
     "\n"
-    "Add a watch to an initialised inotify instance.\n"
+    "Add a watch to an inotify instance, or modify an existing watch.\n"
     "\n"
     "        fd: file descriptor returned by init()\n"
     "        path: path to watch\n"
     "        mask: mask of events to watch for\n"
     "\n"
-    "Return a numeric watch descriptor.");
+    "Return a unique numeric watch descriptor for the inotify instance\n"
+    "mapped by the file descriptor.");
+
+static PyObject *remove_watch(PyObject *self, PyObject *args)
+{
+    PyObject *ret = NULL;
+    uint32_t wd;
+    int fd;
+    int r;
+    
+    if (!PyArg_ParseTuple(args, "iI:remove_watch", &fd, &wd))
+	goto bail;
+
+    r = inotify_rm_watch(fd, wd);
+    if (r == -1) {
+	PyErr_SetFromErrno(PyExc_OSError);
+	goto bail;
+    }
+    
+    Py_INCREF(Py_None);
+    
+    goto done;
+    
+bail:
+    Py_XDECREF(ret);
+    
+done:
+    return ret;
+}
+
+PyDoc_STRVAR(
+    remove_watch_doc,
+    "remove_watch(fd, wd)\n"
+    "\n"
+    "        fd: file descriptor returned by init()\n"
+    "        wd: watch descriptor returned by add_watch()\n"
+    "\n"
+    "Remove a watch associated with the watch descriptor wd from the\n"
+    "inotify instance associated with the file descriptor fd.\n"
+    "\n"
+    "Removing a watch causes an IN_IGNORED event to be generated for this\n"
+    "watch descriptor.");
 
 static PyMethodDef methods[] = {
-    {"init", pyinotify_init, METH_VARARGS, init_doc},
-    {"add_watch", pyinotify_add_watch, METH_VARARGS, add_watch_doc},
+    {"init", init, METH_VARARGS, init_doc},
+    {"add_watch", add_watch, METH_VARARGS, add_watch_doc},
+    {"remove_watch", remove_watch, METH_VARARGS, remove_watch_doc},
     {NULL},
 };
     
